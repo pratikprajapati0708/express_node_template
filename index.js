34e23e03d4683f12ffaeecef6ff2685304b9f113
thecodedguyy <prajapratik1998@gmail.com>
2024-02-14 22:08:30 +0530

    Zod validation basics

---
+++ index.js
@@ -0,0 +1,30 @@
+const express= require('express');
+const app = express();
+const zod = require("zod");
+const schema = zod.array(zod.number());
+
+app.use(express.json())
+
+// const schema = zod.object({
+//     email : zod.string(),
+//     password : zod.string(),
+//      country : z.literal("IN").or(z.literal("US")),
+//     country: z.array(z.number())
+// })
+
+app.post('/heath-checkup',(req,res)=>{
+    const kidneys = req.body.kidneys;
+    const response = schema.safeParse(kidneys);
+    if(response.success){
+        res.send({
+            response
+        })
+    }
+    else{
+        res.status(411).send({
+            message : "Please provide valid inputs"
+        })
+    }
+
+});
+app.listen(3000)
