b91573ebe16c5e376aeeb5e99a1cbee984c15456
thecodedguyy <prajapatipratik292@gmail.com>
2024-02-17 15:18:49 +0530

    added admin signup to db

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,21 @@
 const { Router } = require("express");
 const adminMiddleware = require("../middleware/admin");
+const { Admin } = require("../db");
 const router = Router();
 
 // Admin Routes
-router.post('/signup', (req, res) => {
+router.post('/signup', async(req, res) => {
     // Implement admin signup logic
+    const username = req.body.username;
+    const password = req.body.password;
+
+    await Admin.create({
+        username : username,
+        password : password
+    })
+    res.status(200).json({
+        message: "Admin created successfully"
+    })
 });
 
 router.post('/courses', adminMiddleware, (req, res) => {
